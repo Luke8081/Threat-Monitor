c56131b5e8f754e7b7e715a3afc808539c1b27b5
Luke Allan <luke-allan@outlook.com>
2023-05-10 16:47:42 +0100

    Debugging needed for scan_website function

--- vuln-Assesment.py
+++ vuln-Assesment.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 import time, subprocess, os
+from pprint import pprint
+from zapv2 import ZAPv2
 
 
 class Assesment:
     def __init__(self, addresses):
 
+        self.API_key = ''
         self.addresses = addresses
         self.date = datetime.today().strftime('%Y-%m-%d')
         self.date_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
@@ -12,12 +15,30 @@ class Assesment:
         
     
     def scan_Website(self, address):
+        '''
         #Scan website and record the output in a file
         fileName = f"{address}_{self.date}"
         #Create file
         open("fileName", "w").close()
         to_run = f"./zap.sh {address} -daemon -quickurl -quickout {fileName}.html"
-        subprocess.Popen(to_run)
+        subprocess.Popen(to_run)'''
+
+
+
+        
+        zap = ZAPv2(apikey=self.API_key, proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
+
+        scanID = zap.ascan.scan(address)
+        while int(zap.ascan.status(scanID)) < 100:
+            # Loop until the scanner has finished
+            print('Scan progress %: {}'.format(zap.ascan.status(scanID)))
+            time.sleep(5)
+
+        print('Active Scan completed')
+        # Print vulnerabilities found by the scanning
+        print('Hosts: {}'.format(', '.join(zap.core.hosts)))
+        print('Alerts: ')
+        pprint(zap.core.alerts(baseurl=address))
     
     def scan_Ports():
         pass
@@ -29,8 +50,7 @@ class Assesment:
         for address in self.addresses:
             self.scan_Website(address)
             self.address_counter += 1
-
-        time.sleep(2)
+            
         stop_timer = time.perf_counter()
         self.time = round(stop_timer - start_timer, 2)
     
