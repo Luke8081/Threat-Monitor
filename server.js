838a14daef5415222991656757e433d646714735
Luke Allan <Luke-Allan@outlook.com>
2023-05-23 10:14:01 +0100

    Added nav bar for home page

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var fs = require('fs')
-http.createServer(function (req, res) {
-    fs.readFile('home.html', funtion(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data)
-        return res.end()
-    })
-}).listen(8081);
