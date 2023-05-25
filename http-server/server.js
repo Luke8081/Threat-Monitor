4cd25a855a2c7f2151b36c7dc210891c769c0e6a
Luke Allan <Luke-Allan@outlook.com>
2023-05-25 08:18:57 +0100

    Worked on web page

--- http-server/server.js
+++ http-server/server.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var fs = require('fs');
-
-var app = express.createServer();
+const path = require('path')
+const router = express.Router()
 
 var app = express();
+
 app.get('/', function(req, res) {
-    res.render('home.html', {root: __dirname })
+    //res.sendFile(path.join(__dirname + '/templates/home.html'))
+    var test = "This is a test"
+res.render('/templates/home', {test})
+});
+
+app.get('/addresses', function(req, res) {
+    console.log("Load addresses")
+});
+
+app.get('/run', function(req, res) {
+    console.log('Run button clicked')
+    const to_run = "python3 " + process.cwd().replace("http-server", "vuln-Assesment.py")
+    console.log(to_run)
+    //exec("")
 });
 
 app.listen(8081, '127.0.0.1')
+
+console.log("Waiting on port 8081")
